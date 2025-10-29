0da3befdc649653c15314f59f280e0b192f3612e
Maximus Ponciano <maximus.poncianoo@gmail.com>
2025-10-29 11:45:16 -0300

    Tentando fazer o metodo de pagamento

--- novo_checkout.py
+++ novo_checkout.py
@@ -1,33 +1,36 @@
-#---------------Forma de pagamento------------#
 from abc import ABC, abstractmethod
 
-class pagamento:
+class Pagamento(ABC):
     @abstractmethod
-    def pagar(self, valor_final):
-        pass
-class pagamentoBoleto(pagamento):
-    def pagar(self, valor_final):
-          print(f"Pagamento{valor_final:.2f}feito no boleto")
+    def processarValor(self, valor_final: float) -> bool:
+        ...
+
+class PagamentoPix(Pagamento):
+    def processarValor(self, valor_final: float) -> bool:
+        print(f"Processando R${valor_final:.2f} via PIX...")
+        print("    -> Pagamento com PIX APROVADO (QR Code gerado).")
+        return True 
+
+class PagamentoBoleto(Pagamento):
+    def processarValor(self, valor_final: float) -> bool:
+        print(f"Processando R${valor_final:.2f} via Boleto...")
+        print("    -> Boleto gerado com sucesso.")
+        return True 
+
+class PagamentoCredito(Pagamento):
+    def processarValor(self, valor_final: float) -> bool:
+        print(f"Processando R${valor_final:.2f} via Credito...")
         
+        if valor_final < 1000:
+            print("    -> Pagamento com Credito APROVADO.")
+            return True  
+        else:
+            print("    -> Pagamento com Credito REJEITADO (limite excedido).")
+            return False 
+
+
+pagamento_credito = PagamentoCredito()
     
-class pagamentoPix(pagamento):
-      def pagar(self, valor_final):
-             print(f"Pagamento R$ {valor_final:.2f} feito no pix")
 
-class pagamentoCartao(pagamento):
-      def pagar(self, valor_final):
-              print(f"Processando R${valor_final:.2f} via Cartão de Crédito...")
-
-class pedido:
-    def __init__(self, valor, metodo_pagamento: pagamento):
-        self.valor = valor
-        self.metodo_pagamento = metodo_pagamento
-
-class Desconto:
-    def aplicarDesconto(self, pedido: pedido):
-        if isinstance(pedido.metodo_pagamento, pagamentoPix):
-            desconto = pedido.valor * 0.95
-            pedido.valor = desconto
-            print("Aplicando 5% de desconto PIX.")
-
-            
+pagamento_credito.processarValor(10000)
+ 
